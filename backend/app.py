f22c7a6ab5b923cce79e3efc6e5ef275439228da
Imran Yafith <imranyafith@gmail.com>
2024-06-27 15:54:50 -0400

    refactor: db setup to separate function

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,31 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask
 
+
+def setup_db():
+    db_url = os.environ.get("DATABASE_URL")
+    connection = psycopg2.connect(db_url)
+
+    try:
+        connection = psycopg2.connect(db_url)
+        connection.autocommit = True
+
+        with connection.cursor() as cursor:
+            with open("schema.sql", "r") as schema_file:
+                schema_sql = schema_file.read()
+                cursor.execute(schema_sql)
+                print("Schema executed successfully.")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error executing schema: {error}")
+
+    finally:
+        if connection:
+            connection.close()
+            print("Database connection closed.")
+
+
 load_dotenv()
-
 app = Flask(__name__)
-db_url = os.environ.get("DATABASE_URL")
-connection = psycopg2.connect(db_url)
 
-with connection:
-    connection.autocommit = True
-    with connection.cursor() as cursor:
-        cursor.execute(open("schema.sql", "r").read())
+setup_db()
