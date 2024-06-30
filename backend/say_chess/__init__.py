197feb88d1212b0afc987eb80c0e09aa61cfec15
Imran Yafith <imranyafith@gmail.com>
2024-06-29 20:23:23 -0400

    remove: comments from init file

--- backend/say_chess/__init__.py
+++ backend/say_chess/__init__.py
@@ -5,20 +5,16 @@ from . import db, game, speech
 
 
 def create_app(test_config=None):
-    # Load environment variables from .env file
     load_dotenv()
 
-    # Create and configure the app
     app = Flask(__name__)
     app.config.from_mapping(DATABASE_URL=os.environ.get("DATABASE_URL"))
 
     if test_config:
         app.config.from_mapping(test_config)
 
-    # Initialize the database
     db.init_app(app)
 
-    # Register the game Blueprint
     app.register_blueprint(game.bp, url_prefix="/game")
     app.register_blueprint(speech.bp, url_prefix="/speech")
 
