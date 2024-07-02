3714fce4b775916ccdd4312292346e30d68bc0d5
Imran Yafith <imranyafith@gmail.com>
2024-07-01 21:50:12 -0400

    refactor: error codes to their respective files

--- backend/say_chess/utils.py
+++ backend/say_chess/utils.py
@@ -1,21 +1,6 @@
 from flask import jsonify, make_response
 
-ERROR_CODES = {
-    "PLAYER_IN_GAME": {
-        "code": 1001,
-        "message": "One of the players is already in a game!",
-    },
-    "INVALID_MOVE": {"code": 1002, "message": "Move is invalid!"},
-    "ILLEGAL_MOVE": {"code": 1003, "message": "Move is illegal!"},
-    "AMBIGUOUS_MOVE": {"code": 1004, "message": "Ambiguous move!"},
-    "GAME_NOT_FOUND": {"code": 1005, "message": "Game not found!"},
-    "GAME_ENDED": {"code": 1006, "message": "Game has already ended!"},
-    "GAME_STARTED": {
-        "code": 1007,
-        "message": "Game has already started, cannot abort!",
-    },
-    "UPDATE_FAILED": {"code": 1008, "message": "Failed to update game status!"},
-}
+ERROR_CODES = {}
 
 
 def create_success_response(data, message="Success"):
