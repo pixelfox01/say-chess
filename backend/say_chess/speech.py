59420cf2a016ec4a31993a64e050d7928c475488
Imran Yafith <imranyafith@gmail.com>
2024-07-03 17:52:38 -0400

    update: use fuzzy searching to handle transcript to move conversion

--- backend/say_chess/speech.py
+++ backend/say_chess/speech.py
@@ -1,11 +1,13 @@
-from chess import Move
-from flask import Blueprint, jsonify, request
+from chess import Board
+from flask import Blueprint, request
 from google.cloud import speech
 from say_chess.utils import (
     ERROR_CODES,
     create_error_response,
     create_success_response,
 )
+from fuzzywuzzy import process
+
 
 ERROR_CODES.update(
     {
@@ -21,34 +23,46 @@ ERROR_CODES.update(
 
 ALLOWED_EXTENSIONS = {"wav"}
 
-
-PIECES = ["King", "Queen", "Rook", "Bishop", "Knight", "Pawn"]
-FILES = ["a", "b", "c", "d", "e", "f", "g", "h"]
-RANKS = ["1", "2", "3", "4", "5", "6", "7", "8"]
-
-ALL_MOVES = []
-ALL_COORDS = []
-for piece in PIECES:
-    for rank in RANKS:
-        for file in FILES:
-            move = f"{piece} {file}{rank}"
-            coord = f"{file}{rank}"
-            ALL_MOVES.append(move)
-            ALL_COORDS.append(coord)
-
-
-ALL_MOVES.extend(["Castle short", "Castle long", "Castle kingside", "Castle queenside"])
-ALL_COORDS.extend(["long", "short", "kingside", "queenside"])
-
-
-SPEECH_CONTEXT = speech.SpeechContext(phrases=ALL_MOVES)
+bp = Blueprint("speech", __name__)
 
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-bp = Blueprint("speech", __name__)
+def san_to_spoken(san_move):
+    piece_names = {
+        "K": "King",
+        "Q": "Queen",
+        "R": "Rook",
+        "N": "Knight",
+        "B": "Bishop",
+        "": "",
+    }
+
+    if san_move == "O-O":
+        return "Castle short"
+    elif san_move == "O-O-O":
+        return "Castle long"
+
+    spoken = ""
+    if san_move[0] in piece_names:
+        spoken += piece_names[san_move[0]] + " "
+        san_move = san_move[1:]
+
+    if "=" in san_move:
+        coord, promotion = san_move.split("=")
+        return f"{coord} {piece_names[promotion]}"
+
+    print(spoken + "".join(san_move))
+    return spoken + "".join(san_move)
+
+
+def get_speech_context_from_board(board):
+    legal_moves = list(board.legal_moves)
+    print([board.san(move) for move in legal_moves])
+    spoken_moves = [san_to_spoken(board.san(move)) for move in legal_moves]
+    return speech.SpeechContext(phrases=spoken_moves, boost=20)
 
 
 @bp.route("/transcribe-move", methods=["POST"])
@@ -64,11 +78,20 @@ def transcribe_move():
     if file and allowed_file(file.filename):
         audio_content = file.read()
 
-        transcript = transcribe_gcs(audio_content)
+        fen = request.form.get(
+            "fen",
+            Board(
+                "r1bqkb1r/ppppnppp/5n2/4p3/3NP3/2N5/PPPP1PPP/R1BQKB1R w KQkq - 6 5"
+            ).fen(),
+        )
+        board = Board(fen)
 
-        san_move = get_move_from_transcription(transcript)
+        speech_context = get_speech_context_from_board(board)
+        transcript = transcribe_gcs(audio_content, speech_context)
+
+        san_move = get_move_from_transcription(transcript, board)
         if san_move is None:
-            create_error_response("INVALID_MOVE_TRANSCRIPT", 403)
+            return create_error_response("INVALID_MOVE_TRANSCRIPT", 403)
 
         response = {"transcript": transcript, "san_move": san_move}
 
@@ -77,14 +100,14 @@ def transcribe_move():
         create_error_response("INVALID_FILE_TYPE", 400)
 
 
-def transcribe_gcs(audio_content):
+def transcribe_gcs(audio_content, speech_context):
     client = speech.SpeechClient()
 
     audio = speech.RecognitionAudio(content=audio_content)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         audio_channel_count=1,
-        speech_contexts=[SPEECH_CONTEXT],
+        speech_contexts=[speech_context],
         language_code="en-US",
     )
 
@@ -94,63 +117,15 @@ def transcribe_gcs(audio_content):
     for result in response.results:
         transcript += result.alternatives[0].transcript
 
-    return transcript
+    return transcript.lower()
 
 
-def get_move_from_transcription(transcription):
-    """
-    Get move in SAN notation from gcs transcription.
-    Transcribed move text should be in the form: <piece> <file>rank
-    """
-    san_mappings = {
-        "king": "K",
-        "queen": "Q",
-        "rook": "R",
-        "bishop": "B",
-        "knight": "N",
-        "pawn": "",
-        "castle": "o-o",
-    }
+def get_move_from_transcription(transcription, board):
+    legal_moves = [board.san(move) for move in board.legal_moves]
+    spoken_moves = [san_to_spoken(move).lower() for move in legal_moves]
 
-    transcription = transcription.lower()
-    split_transcription = transcription.split()
+    best_match = process.extractOne(transcription, spoken_moves)
 
-    if len(split_transcription) == 1 and split_transcription[0] in ALL_COORDS:
-        return split_transcription[0]
-
-    if len(split_transcription) != 2:
-        return None
-
-    piece = split_transcription[0]
-    coord = split_transcription[1]
-
-    if piece in [
-        "a8",
-        "b8",
-        "c8",
-        "d8",
-        "e8",
-        "f8",
-        "g8",
-        "h8",
-    ] and coord in PIECES.remove("Pawn"):
-        return f"{piece}={coord}"
-
-    if piece not in san_mappings.keys():
-        return None
-
-    if coord not in ALL_COORDS:
-        return None
-
-    san_move = transcription
-
-    if piece == "castle":
-        if coord == "short" or coord == "kingside":
-            return "O-O"
-        elif coord == "long" or coord == "queenside":
-            return "O-O-O"
-
-    san_move = san_move.replace(piece, san_mappings[piece])
-    san_move = san_move.replace(" ", "")
-
-    return san_move
+    if best_match and best_match[1] > 80:
+        return legal_moves[spoken_moves.index(best_match[0])]
+    return None
